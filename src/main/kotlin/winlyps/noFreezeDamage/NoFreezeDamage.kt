2eb9d5181f62084c9f9bf70a1f184e062d579061
Winlypse <nts3@o2.pl>
2024-10-22 17:56:45 +0200

    Changes!

--- src/main/kotlin/winlyps/noFreezeDamage/NoFreezeDamage.kt
+++ src/main/kotlin/winlyps/noFreezeDamage/NoFreezeDamage.kt
@@ -6,9 +6,14 @@ class NoFreezeDamage : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("freezeDamage", "false")
+        }
+        logger.info("NoFreezeDamage plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoFreezeDamage plugin has been disabled.")
     }
 }
