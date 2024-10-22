a22fff80214b0bf97fea446635df0664be744768
Winlypse <nts3@o2.pl>
2024-10-22 17:56:35 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noFreezeDamage/NoFreezeDamage.kt
@@ -0,0 +1,14 @@
+package winlyps.noFreezeDamage
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoFreezeDamage : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
